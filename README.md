e3fb061a2ef659ad555c3350b3eeb2ca6c8f3cd3
DsnTgr <stevendisontangor@outlook.com>
2021-11-28 20:26:49 +0800

    add the code.

---
+++ README.md
@@ -0,0 +1,6 @@
+React-Snowpack-Electron-Typescript
+====
+It's the demo that build a desktop application with Svelte, Snowpack, Electron and Typescript. 
+
+### CLI
+use `npm install` at first. then you can use `pnpm` command. Because svelte and @snowpack/plugin-svelte that got by pnpm will throw error. 
---
+++ src/render/App.svelte
@@ -0,0 +1,24 @@
+<!-- App.svelte -->
+<script lang="ts">
+    import {onMount} from 'svelte'
+
+    let count: number = 0
+    onMount(() => {
+        const interval = setInterval(() => count++, 1000)
+        return () => {
+            clearInterval(interval)
+        }
+    })
+</script>
+<style>
+    /* css will go here */
+</style>
+<div class="App">
+    <header class="App-header">
+        <p>Edit <code>src/App.svelte</code> and save to reload.</p>
+        <p>Page has been open for <code>{count}</code> seconds.</p>
+        <a class="App-Link" href="https://svelte.dev" target="_blank" rel="noopener noreferrer">
+            Learn Svelte
+        </a>
+    </header>
+</div>
